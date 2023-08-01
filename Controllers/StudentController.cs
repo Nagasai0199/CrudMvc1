8415741d1441fec899bd0d4920b2f9bde7bb8ddf
heman <heman@LAPTOP-V4CNAQSJ>
2023-08-01 11:29:32 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,95 @@
+﻿using CrudMvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMvc1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        saiEntities2 db = new saiEntities2();
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(db.Students.SingleOrDefault(x => x.id == id));
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Students.Add(s);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(db.Students.SingleOrDefault(x => x.id == id));
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Student s)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                db.Entry(s).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(db.Students.SingleOrDefault(x => x.id == id));
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Student student = db.Students.SingleOrDefault(x =>x.id == id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudMvc1.MvcApplication" Language="C#" %>
